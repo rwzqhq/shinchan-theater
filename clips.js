83e4e0bc8435b2167000a3d50d65fd06442c4df2
RuanZH <48212888+rwzqhq@users.noreply.github.com>
2026-09-21 16:13:29 +0800

    Add files via upload

---
+++ clips.js
@@ -0,0 +1,56 @@
+window.CLIPS = [
+  {
+    bvid: "BV1yW42197DR",
+    title: "蜡笔小新的骚操作",
+    up: "Crayon_小鑫",
+    seconds: 154,
+    views: 1522065,
+    pic: "https://i1.hdslb.com/bfs/archive/17c9dfcd4206d8bd0b2ffc9d71e585dd8c9ef61e.jpg",
+    note: "被做成无数表情包的一段"
+  },
+  {
+    bvid: "BV1UF41197wJ",
+    title: "每天一遍，拒绝 emo",
+    up: "陨落之弓",
+    seconds: 157,
+    views: 986508,
+    pic: "https://i1.hdslb.com/bfs/archive/17a726b1d48d2511219370a0399ab2780bc3bfd2.jpg",
+    note: "看完心情会变好"
+  },
+  {
+    bvid: "BV1Zs4y197X1",
+    title: "オラはにんきもの · 我最受欢迎",
+    up: "likejshy",
+    seconds: 214,
+    views: 117619,
+    pic: "https://i0.hdslb.com/bfs/archive/700cb059be9e6e06af427b25ebbcdd75c8d43914.jpg",
+    note: "经典片头曲完整版"
+  },
+  {
+    bvid: "BV1N8411C7h4",
+    title: "蜡笔小新的搞笑名场面",
+    up: "Crayon_小鑫",
+    seconds: 213,
+    views: 189302,
+    pic: "https://i2.hdslb.com/bfs/archive/643d678c50f13d14b7f9a548d5e7964672746334.jpg",
+    note: "名场面连播"
+  },
+  {
+    bvid: "BV1GM4y1c7zh",
+    title: "小 新 的 可 爱 语 录",
+    up: "我是风口的风",
+    seconds: 174,
+    views: 189734,
+    pic: "https://i1.hdslb.com/bfs/archive/2511e933a3021ba0e7bc699744e29bd1d1612756.png",
+    note: "五岁小朋友的清醒发言"
+  },
+  {
+    bvid: "BV15N411W7Bj",
+    title: "新酱日常合集",
+    up: "一个小新酱",
+    seconds: 89,
+    views: 306408,
+    pic: "https://i1.hdslb.com/bfs/archive/1d1033186206a33d6feccd4c9b85e9b8f86e2533.jpg",
+    note: "最短的一段，一分半"
+  }
+];
